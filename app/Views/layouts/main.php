9cd14898618c078165a9bbdc52ab144f53d5b392
FERDY RAHMAT RAMDANI <9088157-ferdyrahmat@users.noreply.gitlab.com>
2024-04-01 11:31:30 +0700

    Setup project

---
+++ app/Views/layouts/main.php
@@ -0,0 +1,47 @@
+<?php
+include "../../../config/index.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIM Skripsi</title>
+    <!-- Description -->
+    <meta name="description" content="Sistem Informasi Manajemen Skripsi">
+    <!-- Keywords -->
+    <meta name="keywords" content="Sistem Informasi, Manajemen Skripsi">
+    <!-- Author -->
+    <meta name="author" content="Sistem Informasi Manajemen Skripsi">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="<?= BASE_URL . "public/img/favicon.png" ?>">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/bower_components/bootstrap/dist/css/bootstrap.min.css" ?>">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/bower_components/font-awesome/css/font-awesome.min.css" ?>">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/bower_components/Ionicons/css/ionicons.min.css" ?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/dist/css/AdminLTE.min.css" ?>">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/dist/css/skins/_all-skins.min.css" ?>">
+    <!-- Morris chart -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/bower_components/morris.js/morris.css" ?>">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/bower_components/jvectormap/jquery-jvectormap.css" ?>">
+    <!-- Date Picker -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css" ?>">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/bower_components/bootstrap-daterangepicker/daterangepicker.css" ?>">
+    <!-- bootstrap wysihtml5 - text editor -->
+    <link rel="stylesheet" href="<?= BASE_URL . "vendor/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" ?>">
+</head>
+
+<body>
+
+</body>
+
+</html>
